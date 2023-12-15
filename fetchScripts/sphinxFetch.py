63ba31757a46cb6b3e52a504ee804e9b342c859e
Blue Robin <julianjohnt@gmail.com>
2023-12-14 22:59:06 -0600

    change to vercel package

--- fetchScripts/sphinxFetch.py
+++ fetchScripts/sphinxFetch.py
@@ -5,5 +5,9 @@ baseURL = "http://165.227.37.175/callers/browse/214-218-3797"
 
 req = requests.get(url=baseURL + "214-218-3797")
 soup = BeautifulSoup(req.text, features="html.parser")
-res = [p.text.strip() for p in soup.select('p')]  # https://stackoverflow.com/questions/40716272/how-to-extract-h1-tag-text-with-beautifulsoup
-print(res)
+res = [p.text.strip() for p in
+       soup.select('p')]  # https://stackoverflow.com/questions/40716272/how-to-extract-h1-tag-text-with-beautifulsoup
+location_info = list(filter(None, [parse.replace('\r', '').replace('\t', '').strip() for parse in res[5].split(
+    '\n')]))  # https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+
+print(location_info)
