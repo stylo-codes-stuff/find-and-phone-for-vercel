2164559b39ab85c73be40804a80d4245953ba631
Blue Robin <julianjohnt@gmail.com>
2023-12-14 22:22:15 -0600

    attribution added

--- fetchScripts/sphinxFetch.py
+++ fetchScripts/sphinxFetch.py
@@ -5,5 +5,5 @@ baseURL = "http://165.227.37.175/callers/browse/214-218-3797"
 
 req = requests.get(url=baseURL + "214-218-3797")
 soup = BeautifulSoup(req.text, features="html.parser")
-res = soup.select('h1')[0].text.strip()
+res = soup.select('h1')[0].text.strip()  # https://stackoverflow.com/questions/40716272/how-to-extract-h1-tag-text-with-beautifulsoup
 print(res)
