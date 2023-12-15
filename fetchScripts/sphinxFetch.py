5f1334dac904ba2cb94a1d1b980f07db0aa3b1de
Blue Robin <julianjohnt@gmail.com>
2023-12-14 22:21:58 -0600

    minor text fix

--- fetchScripts/sphinxFetch.py
+++ fetchScripts/sphinxFetch.py
@@ -5,5 +5,5 @@ baseURL = "http://165.227.37.175/callers/browse/214-218-3797"
 
 req = requests.get(url=baseURL + "214-218-3797")
 soup = BeautifulSoup(req.text, features="html.parser")
-res = soup.select('h1')[0]
+res = soup.select('h1')[0].text.strip()
 print(res)
