5f9f92a57e5f6bc03dc7fd0f0b8e6860505004e5
Blue Robin <julianjohnt@gmail.com>
2023-12-14 21:49:49 -0600

    error handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import flask
 from flask import Flask
 from flask import request
 from flask import render_template
 
-import phoneNumbers
+import phoneNumbers  # handler for phonenumbers
+import phonenumbers  # lib used in phoneNumbers
 
 app = Flask(__name__)
 
@@ -17,9 +19,15 @@ def main():
     return render_template('base.html', name='Input Phone Number Below!', title='Phone Number Information')
 
 
+# noinspection PyBroadException
 @app.route('/numberInfo', methods=['GET', 'POST'])
 def numberInfo():
-    return phoneNumbers.getinformation(str(list(request.form.values())[0]))
+    try:
+        return phoneNumbers.getinformation(str(list(request.form.values())[0]))
+
+    except Exception as e:
+        return f"Unknown error, or the phone number was incorrectly entered. <br> Error: <code>{e}<code> <br> <a href={flask.url_for('main')}>Link To Go Back</a>"
+        # https://stackoverflow.com/questions/27539309/how-do-i-create-a-link-to-another-html-page
 
 
 if __name__ == '__main__':
