5067e4b945a9a8f84a9d847f10b8a77f67c2c316
Blue Robin <julianjohnt@gmail.com>
2023-12-14 22:20:05 -0600

    quick fix for requests

--- fetchScripts/sphinxFetch.py
+++ fetchScripts/sphinxFetch.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 baseURL = "http://165.227.37.175/callers/browse/214-218-3797"
 
-
-req = requests.get(url = baseURL+"214-218-3797")    
-res = req.text()
-print(req)
+req = requests.get(url=baseURL + "214-218-3797")
+soup = BeautifulSoup(req.text, features="html.parser")
+res = soup.select('h1')[0]
+print(res)
