51b4127074b81e644b5e3d74235d35f2d426d3b8
stylo-codes-stuff <>
2023-12-18 23:25:17 +0000

    Fetches info from the sphinx number tracker

--- fetchScripts/sphinx.py
+++ fetchScripts/sphinx.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-
 baseURL = "http://165.227.37.175/callers/browse/"
 new_list = []
 def sphinxFetch(number):
+    new_list.clear()
+    print(number)
     req = requests.get(url=baseURL + number)
+    print(baseURL+number)
     soup = BeautifulSoup(req.text, features="html.parser")
     res = [p.text.strip() for p in soup.select('p')]  # https://stackoverflow.com/questions/40716272/how-to-extract-h1-tag-text-with-beautifulsoup
     for x in res:
@@ -12,5 +14,5 @@ def sphinxFetch(number):
         x = x.replace("\t","")
         x = x.replace("  ","")
         new_list.append(x)
-    return str(new_list)
-print(sphinxFetch("580-982-7133"))
+    print(new_list)
+    return new_list
